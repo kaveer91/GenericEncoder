698ea00c53f4ef891c30b07aad077e4b304a2dcf
kaveer91 <104764046+kaveer91@users.noreply.github.com>
2022-05-03 09:50:58 +0530

    Update EncoderTask.cpp

--- GenericEncoder/Encoder/src/EncoderTask.cpp
+++ GenericEncoder/Encoder/src/EncoderTask.cpp
@@ -1,4 +1,8 @@
 #include "EncoderTask.h"
+#include "Logger.h"
+#include <exception>
+
+#define TRACE_CLASS Logger::TRACE_CLASS_ENCODERTASK
 
 EncoderTask::EncoderTask(IEncoder* iEncoder, const std::string& inputFileName, const std::string& outputFileName):
 iEncoder(iEncoder), inputFileName(inputFileName), outputFileName(outputFileName)
@@ -8,5 +12,12 @@ iEncoder(iEncoder), inputFileName(inputFileName), outputFileName(outputFileName)
 
 void EncoderTask::Execute()
 {
-	iEncoder->Encode(inputFileName, outputFileName);
+	try 
+	{
+	    iEncoder->Encode(inputFileName, outputFileName);
+	}
+	catch(const std::exception& xception)
+	{
+		LOG(Logger::LOG_LEVEL_FATAL, std::string("Exception: ") + xception.what() + ": caught while encoding file " +inputFileName);
+	}
 }
