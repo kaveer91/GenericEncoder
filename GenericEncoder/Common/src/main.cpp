37dbde3d6df85e81d56b4f8816e2a24a3dc3d34f
kaveer91 <104764046+kaveer91@users.noreply.github.com>
2022-05-03 09:47:27 +0530

    Update main.cpp

--- GenericEncoder/Common/src/main.cpp
+++ GenericEncoder/Common/src/main.cpp
@@ -17,17 +17,32 @@ tS32 main(tS32 argumentCount, char* argument[])
 	{
         LOG(Logger::LOG_LEVEL_DEBUG, "valid number of arguments");
 		 
+		std::vector<std::string> listOfMatchedFiles;
+		try
+		{
 		//1.Read files from directory
-		std::string directoryName = argument[1];
-		std::shared_ptr<IReadFiles> iReadFiles = std::make_shared<ReadFilesFromDirectory>();
-		std::vector<std::string> listOfMatchedFiles = iReadFiles->Read(directoryName, EXT_WAV);
+		    std::string directoryName = argument[1];
+		    std::shared_ptr<IReadFiles> iReadFiles = std::make_shared<ReadFilesFromDirectory>();
+		    listOfMatchedFiles = iReadFiles->Read(directoryName, EXT_WAV);
+		}
+		catch(const std::exception& xception)
+		{
+			LOG(Logger::LOG_LEVEL_FATAL, std::string("Exception caught while reading files from directory ") + xception.what());
+		}
 		
 		//2.Encode the files read using 
 		if(listOfMatchedFiles.size() > 0)
 		{
-			LOG(Logger::LOG_LEVEL_INFO, "Found " + std::to_string(listOfMatchedFiles.size())+" files for encoding");
-		    std::shared_ptr<IEncoder> iEncoder = std::make_shared<LameWavToMp3Encoder>();	
-		    iEncoder->Encode(listOfMatchedFiles);
+			try
+			{
+			    LOG(Logger::LOG_LEVEL_INFO, "Found " + std::to_string(listOfMatchedFiles.size())+" files for encoding");
+		        std::shared_ptr<IEncoder> iEncoder = std::make_shared<LameWavToMp3Encoder>();	
+		        iEncoder->Encode(listOfMatchedFiles);
+			}
+			catch(const std::exception& xception)
+		    { 
+    			LOG(Logger::LOG_LEVEL_FATAL, std::string("Exception caught while encoding files : ") + xception.what());
+	    	}
 		}
 		else
 		{
