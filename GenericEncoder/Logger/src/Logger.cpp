3a1a795518284552dd486f935650159351e874fc
kaveer91 <104764046+kaveer91@users.noreply.github.com>
2022-05-03 09:50:27 +0530

    Update Logger.cpp

--- GenericEncoder/Logger/src/Logger.cpp
+++ GenericEncoder/Logger/src/Logger.cpp
@@ -4,7 +4,7 @@ namespace Logger
 {
 	const std::array<std::string, LOG_LEVEL_MAX> logLevelString{"DEBUG","INFO","ERROR", "FATAL"};
 	
-	const std::array<std::string, TRACE_CLASS_MAX+1> traceClassString{"MAIN","READFILES","READFILESFROMDIRECTORY","WRITEFILESTODIRECTORY","WAVTOMP3ENCODER","LAMEWAVTOMP3ENCODER","THREADPOOL"};
+	const std::array<std::string, TRACE_CLASS_MAX+1> traceClassString{"MAIN","READFILES","READFILESFROMDIRECTORY","WRITEFILESTODIRECTORY","WAVTOMP3ENCODER","LAMEWAVTOMP3ENCODER","THREADPOOL","ENCODERTASK"};
 }
 
 Logger::LogLevel enabledLogLevel = Logger::LOG_LEVEL_INFO;
